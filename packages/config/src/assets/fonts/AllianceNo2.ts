08e94c2890b6111e913ab9fe50fad64df3bc5e03
bejzik8 <bejzik8@gmail.com>
2023-02-18 13:46:21 +0100

    🍏 Added Alliance font.

---
+++ packages/config/src/assets/fonts/AllianceNo2.ts
@@ -0,0 +1,116 @@
+import { css } from 'styled-components'
+
+import AllianceNo2Light from './AllianceNo2/AllianceNo2-Light.woff2'
+import AllianceNo2LightItalic from './AllianceNo2/AllianceNo2-LightItalic.woff2'
+import AllianceNo2Regular from './AllianceNo2/AllianceNo2-Regular.woff2'
+import AllianceNo2RegularItalic from './AllianceNo2/AllianceNo2-RegularItalic.woff2'
+import AllianceNo2Medium from './AllianceNo2/AllianceNo2-Medium.woff2'
+import AllianceNo2MediumItalic from './AllianceNo2/AllianceNo2-MediumItalic.woff2'
+import AllianceNo2SemiBold from './AllianceNo2/AllianceNo2-SemiBold.woff2'
+import AllianceNo2SemiBoldItalic from './AllianceNo2/AllianceNo2-SemiBoldItalic.woff2'
+import AllianceNo2Bold from './AllianceNo2/AllianceNo2-Bold.woff2'
+import AllianceNo2BoldItalic from './AllianceNo2/AllianceNo2-BoldItalic.woff2'
+import AllianceNo2ExtraBold from './AllianceNo2/AllianceNo2-ExtraBold.woff2'
+import AllianceNo2ExtraBoldItalic from './AllianceNo2/AllianceNo2-ExtraBoldItalic.woff2'
+import AllianceNo2Black from './AllianceNo2/AllianceNo2-Black.woff2'
+import AllianceNo2BlackItalic from './AllianceNo2/AllianceNo2-BlackItalic.woff2'
+
+export const FontAllianceNo2 = css`
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2Light}) format('woff2');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2LightItalic}) format('woff2');
+    font-weight: 300;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2Regular}) format('woff2');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2RegularItalic}) format('woff2');
+    font-weight: 400;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2Medium}) format('woff2');
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2MediumItalic}) format('woff2');
+    font-weight: 500;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2SemiBold}) format('woff2');
+    font-weight: 600;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2SemiBoldItalic}) format('woff2');
+    font-weight: 600;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2Bold}) format('woff2');
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2BoldItalic}) format('woff2');
+    font-weight: 700;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2ExtraBold}) format('woff2');
+    font-weight: 800;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2ExtraBoldItalic}) format('woff2');
+    font-weight: 800;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2Black}) format('woff2');
+    font-weight: 900;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'AllianceNo2';
+    src: local('AllianceNo2'), local('AllianceNo2'), url(${AllianceNo2BlackItalic}) format('woff2');
+    font-weight: 900;
+    font-style: italic;
+  }
+`

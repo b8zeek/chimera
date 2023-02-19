d2cb25f2fe156271b94e2c8378e5131ab329c04e
bejzik8 <bejzik8@gmail.com>
2023-02-19 22:56:48 +0100

    🐢 Updated POAPsSection component.

--- packages/basic/src/components/POAPsSection.tsx
+++ packages/basic/src/components/POAPsSection.tsx
@@ -2,17 +2,51 @@ import styled from 'styled-components'
 
 import { usePOAPs } from '../hooks/usePOAPs'
 
+type POAP = {
+  chain: string
+  created: string
+  event: {
+    city: string
+    country: string
+    description: string
+    end_date: string
+    event_url: string
+    expiry_date: string
+    fancy_id: string
+    id: number
+    image_url: string
+    name: string
+    start_date: string
+    supply: number
+    year: number
+  }
+  owner: string
+  tokenId: string
+}
+
 export function POAPsSection() {
   const { data, isFetching } = usePOAPs()
 
-  console.log('DATA', data)
+  // TODO: HANDLE POAPs LOADING...
+  if (isFetching) return null
 
-  if (isFetching) return <p>Loading POAPs Section...</p>
-
-  return <Container>POAPs Section</Container>
+  return (
+    <Container>
+      {data.slice(0, 6).map((poap: POAP) => (
+        <POAPToken src={poap.event.image_url} />
+      ))}
+    </Container>
+  )
 }
 
 const Container = styled.section`
   min-height: 20px;
+  padding: 10px;
+  border-radius: 1000px;
   background-color: black;
+`
+
+const POAPToken = styled.img`
+  width: 20%;
+  margin-right: -4%;
 `

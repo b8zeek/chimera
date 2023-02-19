147aa3766c15432bf4a5dfe100ffecc3c81cea2f
bejzik8 <bejzik8@gmail.com>
2023-02-19 15:00:21 +0100

    🫑 Updated About Me section.

--- packages/basic/src/components/MainSection.tsx
+++ packages/basic/src/components/MainSection.tsx
@@ -1,14 +1,26 @@
+import styled from 'styled-components'
+
 import { MainSectionContainer, Paragraph, SubHeading } from '@basic/components'
 
 export function MainSection() {
   return (
     <MainSectionContainer>
-      <SubHeading marginBottom='10px'>About Me</SubHeading>
+      <SubHeading>About Me</SubHeading>
+      <HL />
       <Paragraph color='#fff' textAlign='justify'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-        a type specimen book.
+        I LOVE what I do! I am a full-stack software engineer who takes pride in writing extremely clean, readable and
+        minimal code and focuses on crafting future-proof web applications which are performant and easy to maintain. I
+        value well-organized project structure, thoughtful interactions and innovative ideas. Really passionate about
+        technology, I'm interested in the entire spectrum but my go-to technology is React. I devoted a lot of time
+        getting to know it in-depth together with its whole ecosystem and I enjoy using it the most since it is
+        unopinionated and everyone has a chance to adapt it to their own needs and express themselves in their own way.
+        My abundant energy fuels me in the pursuit of many interests, I enjoy using my obsessive attention to detail, my
+        unequivocal love for improvement and my mission-driven work ethic to continuously get better.
       </Paragraph>
     </MainSectionContainer>
   )
 }
+
+const HL = styled.hr`
+  margin-bottom: 10px;
+`

0b8991c5b3fcc25d70f227c90be3d8c2d0eecafd
bejzik8 <bejzik8@gmail.com>
2023-02-24 14:43:28 +0100

    🧃 Added Framer Motion, updated Heading component.

--- packages/components/src/atoms/Heading.tsx
+++ packages/components/src/atoms/Heading.tsx
@@ -6,11 +6,12 @@ type HeadingProps = {
 }
 
 export const Heading = styled.h1<HeadingProps>`
-  line-height: 55px;
+  line-height: 36px;
   font-size: 40px;
   font-family: AllianceNo2;
   font-weight: 900;
   letter-spacing: -0.03em;
   color: ${({ color }) => color || '#000000'};
   ${({ textAlign }) => textAlign && `text-align: ${textAlign};`}
+  margin-bottom: 20px;
 `
